68a1bef1eb7afb79ef0ae88cf6248d3e69074448
Joan Salas Silva <joan.salas.silva@gmail.com>
2026-03-20 16:09:39 -0300

    Finalizacion principio open and close

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,8 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
 
+// Patron adaptador
 export class HttpClient {
+  // async get(url: string) {
+  //   const { data, status } = await axios.get(url);
+  //   return { data, status };
+  // }
+
   async get(url: string) {
-    const { data, status } = await axios.get(url);
+    const resp = await fetch(url);
+    const data = await resp.json();
+    const status = resp.status;
+
     return { data, status };
   }
 }
